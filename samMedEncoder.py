1d6b5e8e6b746bda6a7a90fc3da3cf9f7f0d4ffb
Kate (PhD) <katephd@Kates-MBP.lan>
2025-03-25 10:34:16 +0000

    Added SAMMedEncoder

---
+++ samMedEncoder.py
@@ -0,0 +1,87 @@
+import copy
+
+import numpy as np
+import torch
+import torch.utils.data as data
+from torch.utils.data.dataset import Dataset
+from segment_anything.build_sam import sam_model_registry
+from segment_anything.predictor_sammed import SammedPredictor
+from argparse import Namespace
+
+import pickle as pkl
+import os
+
+
+class SamMedEncoder:
+    def __init__(self, data, start_idx, dataset_name, checkpoint_path):
+        # check that the dataset parameter is an instance of Dataset
+        assert isinstance(data, Dataset), "train_data is not an instance of Dataset"
+
+        # check that the start_idx parameter is an integer
+        assert isinstance(start_idx, int), "start_idx is not an integer"
+
+        # check that the dataset_name parameter is a string
+        assert isinstance(dataset_name, str), "dataset_name is not a string"
+
+
+        # check that the checkpoint_path parameter is a string
+        assert isinstance(checkpoint_path, str), "checkpoint_path is not a string"
+
+        # check that the checkpoint_path parameter is a valid path
+        assert os.path.exists(checkpoint_path), "checkpoint_path does not exist"
+
+        # check that the checkpoint_path parameter ends with .pth
+        assert checkpoint_path[-4:] == ".pth", "checkpoint_path does not end with .pth"
+
+        self.data = data
+        self.start_idx = start_idx
+        self.dataset_name = dataset_name
+        self.checkpoint_path = checkpoint_path
+
+        # create a directory to store encodings
+        if not os.path.exists("SAMMedEncodings"):
+            os.mkdir("SAMMedEncodings")
+
+        self.representations_dir = os.path.join("SAMMedEncodings", f"{self.dataset_name}")
+        if not os.path.exists(self.representations_dir):
+            os.mkdir(self.representations_dir)
+
+        # set up a data loader. batch size must be 1 for SAMMed Encoder
+        self.data_loader = torch.utils.data.DataLoader(self.data, batch_size=1, shuffle=False)
+
+    def encode(self):
+        args = Namespace()
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        args.image_size = 256
+        args.encoder_adapter = True
+        args.sam_checkpoint = self.checkpoint_path
+        model = sam_model_registry["vit_b"](args).to(device)
+        predictor = SammedPredictor(model)
+
+        for k, (image, _) in enumerate(self.data_loader):
+            if k >= self.start_idx:
+                print(f"Encoding {self.dataset_name} image {k}")
+                image_np = np.moveaxis(image.squeeze(dim=0).numpy(), 0, 2)
+                predictor.set_image(image_np)
+                embedding = predictor.get_image_embedding()
+
+                # check the embedding size
+                print("Embedding size: ", embedding.shape)
+
+                f = open(os.path.join(self.representations_dir, "img_{}.pkl".format(k)), "wb")
+                pkl.dump(embedding.flatten().unsqueeze(0), f)
+                f.close()
+
+    def retrieve(self, indices):
+        # retrieve pre-computed embeddings based on a list of indicies
+        for p in range(indices.shape[0]):
+            f = open(os.path.join(self.representations_dir, "img_{}.pkl".format(indices[p])), "rb")
+            embedding = pkl.load(f)
+            f.close()
+
+            if p == 0:
+                vectors = copy.deepcopy(embedding)
+            else:
+                vectors = torch.cat((vectors, embedding), dim=0)
+
+        return vectors.detach().numpy()
---
+++ storeSAMMedEncodings.py
@@ -0,0 +1,27 @@
+# iterate across a dataset and save the SAMMed encodings
+import argparse
+from samMedEncoder import SamMedEncoder
+import os
+import torchvision.transforms as transforms
+from cheXpertDataset import CheXpertDataset
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Precompute and store SAMMed Encodings for a dataset")
+parser.add_argument("-r", "--root_dir", type=str, help="Root directory where the code and data are located",
+                    default="/Users/katephd/Documents")
+#parser.add_argument("-d", "--dataset_name", type=str, help="Name of dataset.", default="breastmnist")
+#parser.add_argument("-i", "--image_size", type=int, help="Size of the images", default=28)
+parser.add_argument("-s", "--start_idx", type=int, help="Index of the image to start encoding", default=0)
+
+args = parser.parse_args()
+
+root_dir = args.root_dir
+start_idx = args.start_idx
+
+data_dir = os.path.join(root_dir, 'data/CheXpertSmall')
+dataset = CheXpertDataset(data_dir, split='train', transform=transforms.ToTensor())
+
+checkpoint_path = os.path.join("SAMMedCheckpoint/pretrain_model/sam-med2d_b.pth")
+
+encoder = SamMedEncoder(dataset, start_idx, "CheXpert", checkpoint_path)
+encoder.encode()
