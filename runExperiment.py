e20ade3963a56bc24c27a68ca9736c9173496c80
Kate (PhD) <katephd@MacBookPro.lan>
2025-04-17 10:35:28 +0100

    Experiment stops early if we don't have enough data to sample from

--- runExperiment.py
+++ runExperiment.py
@@ -20,11 +20,13 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
         df = pd.read_csv(train_reduced_csv)
     else:
         raise ValueError("Dataset not supported")
-
+    
     for value in values:
-        # create containers to store the diversity scores
+        # create containers to store the diversity scores which are initiated to NaN
         vendi_scores_inception = np.zeros((len(num_samples), num_repeats))
+        vendi_scores_inception.fill(np.nan)
         vendi_scores_sammed = np.zeros((len(num_samples), num_repeats))
+        vendi_scores_sammed.fill(np.nan)
 
         for ns in num_samples:
             for i in range(num_repeats):
@@ -43,14 +45,19 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
                     condition2 = df[demographic] == value
                     image_ids = df[condition1 & condition2]["image_id"].values
 
-                # create ns random indices between 0 and the dataset size
-                idx = np.random.choice(range(image_ids.shape[0]), ns, replace=False)
-                ids = image_ids[idx].astype(int)
+                # check if the number of samples is greater than the number of image IDs
+                # if not, fill diversitys scores with Nan
+                if ns < image_ids.shape[0]:
+                    # create ns random indices between 0 and the dataset size
+                    idx = np.random.choice(range(image_ids.shape[0]), ns, replace=False)
+                    ids = image_ids[idx].astype(int)
 
-                # create a subset of the dataset for diversity scoring
-                ds = DiversityScore(Subset(dataset, ids), ids, {"dataset_name": dataset_name})
+                    # create a subset of the dataset for diversity scoring
+                    ds = DiversityScore(Subset(dataset, ids), ids, {"dataset_name": dataset_name})
 
-                scores = ds.scoreDiversity()
+                    scores = ds.scoreDiversity()
+                else:
+                    scores = {"vs_inception": np.nan, "vs_sammed": np.nan}
 
                 # store the results
                 vendi_scores_inception[num_samples.index(ns), i] = scores["vs_inception"]
@@ -61,11 +68,27 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
                     f = open(f"results_{value}.pkl", "wb")
                     pkl.dump({"vendi_scores_inception": vendi_scores_inception, "vendi_scores_sammed": vendi_scores_sammed}, f)
                     f.close()
+    
 
-    # run mixed experimets (even mix of all of the demographic values)
-    # create containers to store the diversity scores
+    # create containers to store the diversity scores which are initiated to NaN
+    vendi_scores_inception = np.zeros((len(num_samples), num_repeats))
+    vendi_scores_inception.fill(np.nan)
+    vendi_scores_sammed = np.zeros((len(num_samples), num_repeats))
+    vendi_scores_sammed.fill(np.nan)
+
+    # create a flag that will be set when we can no longer run experiments
+    SKIP_FLAG = False
+    
     for ns in num_samples:
+        if SKIP_FLAG:
+            print("Skipping {} samples".format(ns))
+            break
+
         for i in range(num_repeats):
+            if SKIP_FLAG:
+                print("Skipping {} repeats".format(i))
+                break
+
             print("scoring mixed diversity for {} samples, repeat {}".format(ns, i))
             print("We will be using {} samples per demographic value".format(int(ns / len(values))))
 
@@ -77,6 +100,11 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
 
             # loop over the demographic values
             for value in values:
+
+                if SKIP_FLAG:
+                    print("Skipping {} value".format(value))
+                    break
+
                 # filter the dataframe  IDs to only include AP scans from females
                 condition1 = df["AP/PA"] == "AP"
 
@@ -88,6 +116,12 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
                 else:
                     condition2 = df[demographic] == value
                     image_ids = df[condition1 & condition2]["image_id"].values
+
+                # check if the number of samples is greater than the number of image IDs
+                if ns_per_value > image_ids.shape[0]:
+                    print("Not enough samples for demographic value {}. Skipping this repeat".format(value))
+                    SKIP_FLAG = True
+                    break
 
                 # create ns_per_value random indices between 0 and the dataset size for that demographic value
                 idx = np.random.choice(range(image_ids.shape[0]), int(ns_per_value), replace=False)
@@ -115,7 +149,7 @@ def runExperiment(num_samples, num_repeats, demographic, values, dataset_name, r
                 f.close()
 
 def plotResults(values, num_samples, encoder, results_dir):       
-    # Now we can plot the results
+    # Now we can plot the re1sults
     plt.clf()
 
     # load the results
@@ -145,16 +179,19 @@ def plotResults(values, num_samples, encoder, results_dir):
 def main():
     root_dir = '/Users/katephd/Documents/data/'
 
-    num_repeats = 30
-    num_samples = [50, 100, 200, 500, 1000]
+    num_repeats = 3
+    #num_samples = [50, 100, 200, 500, 1000]
+    num_samples = [50, 200, 1000]
     #demographic = "Sex"
     #values = ["Male", "Female"]
-    demographic = "Age"
-    values = ["20-30", "30-40", "40-50", "50-60", "60-70", "70-80"]
+    #demographic = "Age"
+    #values = ["20-30", "30-40", "40-50", "50-60", "60-70", "70-80"]
+    demographic = "Atelectasis"
+    values = [1, 0]
     dataset_name = "CheXpert"
 
     # run the experiment
-    #runExperiment(num_samples, num_repeats, demographic, values, dataset_name, root_dir, save=True)
+    runExperiment(num_samples, num_repeats, demographic, values, dataset_name, root_dir, save=True)
 
     # plot the results
     plotResults(values, num_samples, "inception", ".")
