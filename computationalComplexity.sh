0eadca74337ffeeb4ac401e8140db535d5c29a4b
Kate (PhD) <katephd@Kates-MacBook-Pro.local>
2025-09-30 14:55:44 +0200

    setup for runninng computational complexity experiments on HPC

---
+++ computationalComplexity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=15:mem=80gb:ngpus=1:gpu_type=RTX6000
+
+# bash script to run generalisation experiments on HPC
+cd ${PBS_O_WORKDIR}
+
+# Launch virtual environment
+module load anaconda3/personal
+
+# install requirements
+pip install -r requirements.txt
+
+# run experiments
+python computationalComplexity.py
