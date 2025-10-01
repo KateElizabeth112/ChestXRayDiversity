beca0c4f2fabc1cbab5101ac49cae3705d6b5be1
Kate (PhD) <katephd@Kates-MacBook-Pro.local>
2025-10-01 13:59:01 +0200

    increased memory

--- computationalComplexity.sh
+++ computationalComplexity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
-#PBS -l select=1:ncpus=15:mem=80gb:ngpus=1:gpu_type=RTX6000
+#PBS -l select=1:ncpus=30:mem=150gb
 
 # bash script to run generalisation experiments on HPC
 cd ${PBS_O_WORKDIR}
